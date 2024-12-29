ab83cf4428d0994f12026bfca863414808df9d70
Alexandre <alexandre.delecloz@gmail.com>
2024-12-29 13:15:48 +0100

    Map-Research:3.1 FIX queryCriteria in the research

--- src/main/java/be/ecotravel/back/search/QuerySearchCriteriaStrategy.java
+++ src/main/java/be/ecotravel/back/search/QuerySearchCriteriaStrategy.java
@@ -15,7 +15,34 @@ public class QuerySearchCriteriaStrategy implements SearchCriteriaStrategy {
 
     @Override
     public boolean matches(Destination destination) {
-        return true;
-//        return destination.getName().contains(query) || destination.getDescription().contains(query);
+        //if query is empty, return true
+        if (query == null || query.isEmpty()) return true;
+
+
+
+//        if (destination.getName().contains(query) || destination.getDescription().contains(query)) {
+//            return true;
+//        }
+//        if (destination.getAddress().getCountry().contains(query) || destination.getAddress().getLocation().contains(query)) {
+//            return true;
+//        }
+//        return false;
+
+        //error if a destination.value() is null
+        //correction:
+        if (destination.getName() != null && destination.getName().contains(query)) {
+            return true;
+        }
+        if (destination.getDescription() != null && destination.getDescription().contains(query)) {
+            return true;
+        }
+        if (destination.getAddress() != null) {
+            if (destination.getAddress().getCountry() != null && destination.getAddress().getCountry().contains(query)) {
+                return true;
+            }
+            return destination.getAddress().getLocation() != null && destination.getAddress().getLocation().contains(query);
+        }
+        return false;
+
     }
 }
