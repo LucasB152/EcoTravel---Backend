c89016d5620a9e74cfc61463b76910f1063057af
LucasB <lucasbauduin152@gmail.com>
2025-01-19 23:04:30 +0100

    Add security source

--- src/main/java/be/ecotravel/back/repository/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package be.ecotravel.back.repository;
-
-import be.ecotravel.back.entity.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ReportRepository extends JpaRepository<Report, UUID> {
-}
